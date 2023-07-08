c95517b6d26db73fb1d7c92418c425d01045093b
kody.low <kodylow7@gmail.com>
2023-07-08 10:51:53 -0700

    got proxy working correctly

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/kodylow/actually_openai
 go 1.20
 
 require github.com/gorilla/mux v1.8.0
+
+require github.com/joho/godotenv v1.5.1 // indirect
--- main.go
+++ main.go
@@ -6,10 +6,12 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 const (
-	OpenAIEndpoint = "https://api.openai.com/v1/"
+	OpenAIEndpoint = "https://api.openai.com/"
 )
 
 var (
@@ -17,52 +19,67 @@ var (
 )
 
 func init() {
-	APIKey = os.Getenv("OPENAI_API_KEY")  // Read API key from environment variable
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	APIKey = os.Getenv("OPENAI_API_KEY")
+}
+
+func passthroughHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("passthroughHandler started")
+
+	// Read the body
+	bodyBytes, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Error reading request body:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Create a new request to forward
+	req, err := http.NewRequest(r.Method, OpenAIEndpoint+r.URL.Path, bytes.NewBuffer(bodyBytes))
+	if err != nil {
+		log.Println("Error creating new forward request:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Copy headers
+	for name, values := range r.Header {
+		for _, value := range values {
+			req.Header.Add(name, value)
+		}
+	}
+
+	// Add OpenAI API Key
+	req.Header.Add("Authorization", "Bearer "+APIKey)
+
+	// Forward the request
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Println("Error forwarding the request:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Read the response
+	responseBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Println("Error reading response body:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Write the response back to the client
+	w.Write(responseBody)
+
+	log.Println("passthroughHandler completed")
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Read the body
-		bodyBytes, err := ioutil.ReadAll(r.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	http.HandleFunc("/", passthroughHandler)
 
-		// Create a new request to forward
-		req, err := http.NewRequest(r.Method, OpenAIEndpoint + r.URL.Path, bytes.NewBuffer(bodyBytes))
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Copy headers
-		for name, values := range r.Header {
-			for _, value := range values {
-				req.Header.Add(name, value)
-			}
-		}
-
-		// Add OpenAI API Key
-		req.Header.Add("Authorization", "Bearer "+APIKey)
-
-		// Forward the request
-		resp, err := http.DefaultClient.Do(req)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Read the response
-		responseBody, err := ioutil.ReadAll(resp.Body)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Write the response back to the client
-		w.Write(responseBody)
-	})
-
+	log.Println("Server starting on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
