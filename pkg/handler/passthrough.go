c796caf216b1f44e36ce85b89faf96d40e5d344c
Replit user <>
2023-07-27 05:00:52 +0000

    hold

--- pkg/handler/passthrough.go
+++ pkg/handler/passthrough.go
@@ -32,7 +32,68 @@ func Init(key string, root string, lnAddress string) error {
 func PassthroughHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("passthroughHandler started")
 
-	// (Your code remains the same up to here)
+	// Check if the requested endpoint is supported
+	reqPath := r.URL.Path
+	reqMethod := r.Method
+	var isSupported bool
+	for _, endpoint := range service.SupportedEndpoints {
+		if reqPath == endpoint.Path && reqMethod == endpoint.Method {
+			isSupported = true
+			break
+		}
+	}
+
+	if !isSupported {
+		http.Error(w, "Endpoint not supported", http.StatusNotFound)
+		return
+	}
+
+	// Read the body
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("Error reading body:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	r.Body = io.NopCloser(bytes.NewBuffer(body)) // Reset the body to its original state
+
+	// Create a RequestInfo
+	reqInfo := models.RequestInfo{
+		AuthHeader: r.Header.Get("Authorization"),
+		Method:     r.Method,
+		Path:       r.URL.Path,
+		Body:       body,
+	}
+
+	err = auth.CheckAuthorizationHeader(reqInfo)
+	if err != nil {
+		log.Println("Unauthorized, payment required")
+		l402, err := auth.GetL402(reqInfo)
+		if err != nil {
+			log.Println("Error getting L402:", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		// Send 402 Payment Required with the invoice
+		w.Header().Set("WWW-Authenticate", l402)
+		http.Error(w, "Payment Required", http.StatusPaymentRequired)
+		return
+	}
+
+	// Create a new request to forward
+	req, err := http.NewRequest(reqInfo.Method, APIRoot+reqInfo.Path, r.Body)
+	if err != nil {
+		log.Println("Error creating new forward request:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// overwrite the Authorization with the API Key
+	req.Header.Set("Authorization", "Bearer "+APIKey)
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Accept", "application/json")
+	log.Println("Forwarding request to OpenAI API")
+	log.Println("Request:", req)
 
 	// Forward the request
 	resp, err := http.DefaultClient.Do(req)
